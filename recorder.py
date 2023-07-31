2025b3a5fca8bb336231dccc41923234ca9bc10c
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-07-31 02:05:37 +0100

    Initital commit

---
+++ recorder.py
@@ -0,0 +1,53 @@
+import pyaudio
+import wave
+import numpy as np
+import librosa
+
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 1
+fs = 44100  # Record at 44100 samples per second
+seconds = 3
+filename = "output.wav"
+
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+print('Recording')
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+
+frames = []  # Initialize array to store frames
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+# Stop and close the stream 
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+print('Finished recording')
+
+# Save the recorded data as a WAV file
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+# Load the audio file with librosa
+y, sr = librosa.load(filename, sr=fs)
+
+# Use librosa to calculate the tempo and beat frames
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+print(f"Detected tempo: {tempo}")
+print(f"Beat frames: {beat_frames}")
